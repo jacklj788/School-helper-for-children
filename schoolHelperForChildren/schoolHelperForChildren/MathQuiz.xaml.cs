1e3cbc48071dd62440706101bd389aeb5a45079d
jacklj788 <jackj88@hotmail.co.uk>
2016-11-03 16:25:24 +0000

    16:24 - 03/11/2016 - JLJ : Changed the float to doubles. The equation for random numbers is working, currently only 1 equation is up.

--- schoolHelperForChildren/schoolHelperForChildren/MathQuiz.xaml.cs
+++ schoolHelperForChildren/schoolHelperForChildren/MathQuiz.xaml.cs
@@ -20,24 +20,35 @@ namespace schoolHelperForChildren
     /// </summary>
     public partial class MathQuiz : UserControl
     {
+        double[] answers = new double[1];
         public MathQuiz()
         {
             InitializeComponent();
 
             Random rnd = new Random();
 
-            float[] numbers = new float[2];
+            double[] numbers = new double[2];
             // There will be half as many answers as numbers - it's 1 for now but will grow, which is why its an array
-            float[] answers = new float[1];
 
             // Will put this in a foreach loop later - Declares the variables as random numbers between 1 and 45. 45 is a good max number for children 
             numbers[0] = rnd.Next(1, 45);
-            numbers[1] = rnd.Next(1, 99);
+            numbers[1] = rnd.Next(1, 45);
 
-            answers[0] = numbers[0] + numbers[1];
+            answers[0] = (numbers[0] + numbers[1]);
 
             num1Lbl.Content = numbers[0];
             num2Lbl.Content = numbers[1];
+        }
+
+        private void submitAnswersTxtbox_Click(object sender, RoutedEventArgs e)
+        {
+            // Floats werent working as float parsing was breaking, but double converting is fine. 
+            double textBoxToNumber = Convert.ToDouble(answers[0]);
+            //num1Lbl.Content = textBoxToNumber;
+            if (textBoxToNumber == answers[0])
+            {
+                label.Visibility = Visibility.Visible;
+            }
         }
     }
 }
